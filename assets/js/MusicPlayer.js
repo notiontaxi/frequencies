473e7a6ad70e6fadbda833bd61adfd2c5f24bb78
notiontaxi <florian.wokurka@gmail.com>
2014-05-16 20:42:17 +0200

    impl: audio-player #3

--- assets/js/MusicPlayer.js
+++ assets/js/MusicPlayer.js
@@ -6,16 +6,14 @@ https://github.com/frequencies
 "use strict"
 
 define([
-  'js/Playlist',
   'js/Effects',
   'js/Visualization'
   ], function(
-    Playlist,
     Effects,
     Visualization
   ) {
 
-var MusicPlayer, _ref, module,
+  var MusicPlayer, _ref, module,
 
 
   module = function() {}
@@ -23,18 +21,70 @@ var MusicPlayer, _ref, module,
     //__extends(ImageProcessing, CanvasGui);
 // --------------------------------------
 
-
     function MusicPlayer(){   
 
-      this.playlist = new Playlist(this)
       this.effects = new Effects(this)
       this.visualization = new Visualization(this)
-      
+
+      this.playing = false
+      this.mediaPath = './assets/media/audio/'
+      this.extension = ''
+
       this.initialize()
       //this.runTests()
     }
 
     MusicPlayer.prototype.initialize = function(){
+      this.initializePlayer()
+      this.loadDefaultTracks()
+    }
+
+    MusicPlayer.prototype.initializePlayer = function(){
+
+      this.audio = $("#audio-player")[0]
+
+      var that = this
+      this.audio.addEventListener('play',  function(){that.playAction(that)}, false)
+      this.audio.addEventListener('pause', function(){that.pauseAction(that)}, false)
+      this.audio.addEventListener('ended', function(){that.endedAction(that)}, false)   
+    }
+
+    MusicPlayer.prototype.playAction = function(player){
+      this.playing = true
+      // npAction.text('Now Playing:');
+      console.log("play")
+    }
+
+    MusicPlayer.prototype.pauseAction = function(player){
+      this.playing = true
+      console.log("pause")
+      // npAction.text('Now Playing:');
+    }
+
+    MusicPlayer.prototype.endedAction = function(player){
+      // npAction.text('Now Playing:');
+      // if((index + 1) < trackAmount) {
+      //         index++;
+      //         loadTrack(index);
+      //         audio.play();
+      //       } else {
+      //         audio.pause();
+      //         index = 0;
+      //         loadTrack(index);
+      //       }
+    }
+
+
+    MusicPlayer.prototype.loadDefaultTracks = function(){
+      this.tracks = Array()
+
+      this.tracks.push({
+        track: 1, 
+        name :"001", 
+        length: "00:55", 
+        fileName: "test",
+        extension: "ogg"
+      });
 
     }
 
